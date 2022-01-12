0db84bc609f4c350b74781acb99e632904cd8463
diego-sepulveda-lavin <diegosl@hotmail.cl>
2022-01-12 23:57:38 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Post
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,42 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize_with_posts(), users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(users), 200
 
-    return jsonify(response_body), 200
+@app.route('/users', methods=['POST'])
+def create_new_user():
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if "email" not in body:
+        raise APIException('You need to specify the email', status_code=400)
+    if "password" not in body:
+        raise APIException('You need to specify the password', status_code=400)
+    if "is_active" not in body:
+        raise APIException('You need to specify the is_active', status_code=400)
+
+    new_user = User(email=body['email'], password=body['password'], is_active=body['is_active'])
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Usuario creado exitosamente"}), 201
+
+@app.route('/post/<int:post_id>')
+def get_post_by_id(post_id):
+    post = Post.query.get(post_id)
+    if post:
+        post = post.serialize()
+        return jsonify(post),200
+    return jsonify({"mensaje": "Post no encontrado"}), 404
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,13 +7,38 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    posts = db.relationship('Post', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    def serialize_with_posts(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "is_active": self.is_active,
+            "posts": list(map(lambda post:post.serialize(),self.posts))
+        }
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "user_id": self.user_id
         }
